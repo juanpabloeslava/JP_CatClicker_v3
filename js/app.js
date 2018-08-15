0f2717d945cf20c526b52099a52019ee9a45907b
Juan Pablo Eslava <jpesmar@gmail.com>
2018-08-15 18:36:47 +0200

    Click on list changes Cat (not sure why though)

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ let controller = {
 	// 
 	init: function () {
 		// add 5 cats to model.cats array
-		// in the future this could be set by an alarm with an input
-		// that asks the user how many cats does he want to have
 		this.addCats(5);
 
 		// set the active Cat view
@@ -19,6 +17,7 @@ let controller = {
 
 		// make the list
 		listView.init();
+		catView.init();
 	}, 
 
 	getActiveCat: function () {
@@ -29,96 +28,102 @@ let controller = {
 		return model.cats;
 	},
 
+	changeCat: function (cat) {
+		// alert ('changeCat() was fired from the controller');
+		model.activeCat = cat;
+	},
+
 	// add cats to the list and images according to
 	// the amount of cats stored in images/. Just be sure
 	// to name the images correctly using the following guide:
 	// cat_picture(number).jpg
 	addCats: function (numberOfCats) {
+		// clear cat list
+		model.cats = [];
 		// create an object for each desired cat, and push
 		// it into the cat list (array)
 		for (var i = 1; i < numberOfCats+1; i++) {	
 			model.cats.push({
 				clickCount: 0,
 				name: 'Cat'+[i],
-				imgSrc: 'images/cat_picture'+[i]+'.jpg'
+				imgSrc: 'images/cat_picture'+[i]+'.jpeg'
 			});
 		}
 	}
 };
 
 /* ======= VIEW ========== */
-let listView = {
-	//TO-DO: make a list with the name of the cats
+let catView = {
 
-	// catList: document.getElementById('catlist-view'),
-	
-	// cats: controller.getAllCats(),
-	
 	init: function () {
 		// set DOM variables
+		this.catView = document.getElementById('cat-view');
+		this.catName = document.getElementById('cat-name');
+		this.catCounter = document.getElementById('cat-count');
+		this.catImg = document.getElementById('cat-img');
+		// this.cats = controller.getAllCats();
+		
+		// render for the first time
+		this.render();
+	},
+
+	render: function () {
+		// set the active cat to display name and image
+		let showCat = controller.getActiveCat();
+
+		// do things
+		this.catName.innerHTML = showCat.name;
+		this.catCounter.innerHTML = showCat.clickCount;
+		this.catImg.src = showCat.imgSrc;
+	}
+};
+
+let listView = {
+	//TO-DO: make a list with the name of the cats
+	init: function () {
+		// 
 		this.catList = document.getElementById('catlist-view');
-		this.cats = controller.getAllCats();
 		
 		// render for the first time
 		this.render();
 	},
 	
 	render: function () {
+		// clear cat list
+		this.catList.innerHTML = '';
+		
+		// get all the cats that we're gonna use
+		this.cats = controller.getAllCats();
+
 		// create the cats' list
 		for (let cat of this.cats) {
 			// create and modify <li> items for the cats' list
-			let catListElement = document.createElement('li');
-			catListElement.innerHTML = `See ${cat.name}`;
+			this.catListElement = document.createElement('li');
+			this.catListElement.innerHTML = `See ${cat.name}`;
+			
+			// click on list and change Cat
+			// this.catListElement.addEventListener('click', (function (localCat) {
+			// 	// change the value of the current Cat
+			// 	return function () {
+			// 		controller.changeCat(localCat);
+			// 		catView.render();
+			// 	}
+			// })(cat));
+
+			this.catListElement.addEventListener('click', function (localCat) {
+				// change the value of the current Cat
+				return function () {
+					controller.changeCat(localCat);
+					catView.render();
+				}
+			});
+
 			// add each <li> item to the DOM
-			this.catList.appendChild(catListElement);
+			this.catList.appendChild(this.catListElement);
 		}
 	}
 };
 
-let catView = {
-	
-};
+
 
 controller.init();
-
-
-/*
- * Old shit
- */
-
-
-// var cats = $(".cat");
-// var buttons = $("button");
-
-// function hideAllCats(){
-// 	for (var i=0; i<cats.length; i++){
-// 		$(cats[i]).hide();
-// 	}
-// }
-
-// function bindButtonToCat(idNumber){
-// 	$("#button"+idNumber).click(function(){
-// 		hideAllCats();
-// 		$("#cat"+idNumber).show();
-// 	})
-// }
-
-// function bindCounterToCat(idNumber){
-// 	var cat = "#cat"+idNumber
-// 	$(cat).click(function(){
-// 		var count = $(cat+" > .counter").text();
-// 		count = parseInt(count) + 1;
-// 		$(cat+" > .counter").text(count);
-// 	})
-// }
-
-// for (var i=1; i<=buttons.length; i++){
-// 	bindButtonToCat(i);
-// }
-
-// for (var i=1; i<=cats.length; i++){
-// 	bindCounterToCat(i);
-// }
-
-// hideAllCats();
-// $("#cat1").show();

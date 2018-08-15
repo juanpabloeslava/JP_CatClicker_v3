ae327064cf5edf3f6d872d818a872bc6fd254efd
Juan Pablo Eslava <jpesmar@gmail.com>
2018-08-15 12:48:28 +0200

    Added MVC structure to app

--- js/app.js
+++ js/app.js
@@ -1,36 +1,81 @@
 
-var cats = $(".cat");
-var buttons = $("button");
+// ------------ MODEL -------------
+let model = {
+	// cat variables
+	activeCat: null,
+	cats: [],
+	
+	
+	
+};
 
-function hideAllCats(){
-	for (var i=0; i<cats.length; i++){
-		$(cats[i]).hide();
-	}
-}
+// ------------ CONTROLLER -------------
+let controller = {
+	// 
+	init: function () {
+		// add cats to the list and images according to
+		// the amount of cats stored in images/
+		this.addCats(5);
+	}, 
 
-function bindButtonToCat(idNumber){
-	$("#button"+idNumber).click(function(){
-		hideAllCats();
-		$("#cat"+idNumber).show();
-	})
-}
+	addCats: function (numberOfCats) {
+		// create an object for each desired cat, and push
+		// it into the cat list (array)
+		for (var i = 1; i < numberOfCats+1; i++) {	
+			model.cats.push({
+				clickCount: 0,
+				name: 'Cat'+[i],
+				imgSrc: 'images/cat_picture'+[i]+'.jpg'
+			});
+		}
+	},
+};
 
-function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
-	$(cat).click(function(){
-		var count = $(cat+" > .counter").text();
-		count = parseInt(count) + 1;
-		$(cat+" > .counter").text(count);
-	})
-}
+// ------------ VIEW -------------
+let view = {
 
-for (var i=1; i<=buttons.length; i++){
-	bindButtonToCat(i);
-}
+};
 
-for (var i=1; i<=cats.length; i++){
-	bindCounterToCat(i);
-}
+controller.init();
 
-hideAllCats();
-$("#cat1").show();
+
+/*
+ * Old shit
+ */
+
+
+// var cats = $(".cat");
+// var buttons = $("button");
+
+// function hideAllCats(){
+// 	for (var i=0; i<cats.length; i++){
+// 		$(cats[i]).hide();
+// 	}
+// }
+
+// function bindButtonToCat(idNumber){
+// 	$("#button"+idNumber).click(function(){
+// 		hideAllCats();
+// 		$("#cat"+idNumber).show();
+// 	})
+// }
+
+// function bindCounterToCat(idNumber){
+// 	var cat = "#cat"+idNumber
+// 	$(cat).click(function(){
+// 		var count = $(cat+" > .counter").text();
+// 		count = parseInt(count) + 1;
+// 		$(cat+" > .counter").text(count);
+// 	})
+// }
+
+// for (var i=1; i<=buttons.length; i++){
+// 	bindButtonToCat(i);
+// }
+
+// for (var i=1; i<=cats.length; i++){
+// 	bindCounterToCat(i);
+// }
+
+// hideAllCats();
+// $("#cat1").show();

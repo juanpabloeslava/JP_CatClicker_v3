a1ecda5d28bf5b9a03167114d5c36f76a3a5a9fc
Juan Pablo Eslava <jpesmar@gmail.com>
2018-08-15 14:25:44 +0200

    Added cats list

--- js/app.js
+++ js/app.js
@@ -1,23 +1,38 @@
-
-// ------------ MODEL -------------
+/* ======= MODEL ========== */
 let model = {
 	// cat variables
 	activeCat: null,
 	cats: [],
-	
-	
-	
 };
 
-// ------------ CONTROLLER -------------
+/* ======= CONTROLLER ========== */
 let controller = {
 	// 
 	init: function () {
-		// add cats to the list and images according to
-		// the amount of cats stored in images/
+		// add 5 cats to model.cats array
+		// in the future this could be set by an alarm with an input
+		// that asks the user how many cats does he want to have
 		this.addCats(5);
+
+		// set the active Cat view
+		model.activeCat = model.cats[0];
+
+		// make the list
+		listView.init();
 	}, 
 
+	getActiveCat: function () {
+		return model.activeCat;
+	},
+	
+	getAllCats: function () {
+		return model.cats;
+	},
+
+	// add cats to the list and images according to
+	// the amount of cats stored in images/. Just be sure
+	// to name the images correctly using the following guide:
+	// cat_picture(number).jpg
 	addCats: function (numberOfCats) {
 		// create an object for each desired cat, and push
 		// it into the cat list (array)
@@ -28,12 +43,40 @@ let controller = {
 				imgSrc: 'images/cat_picture'+[i]+'.jpg'
 			});
 		}
-	},
+	}
 };
 
-// ------------ VIEW -------------
-let view = {
+/* ======= VIEW ========== */
+let listView = {
+	//TO-DO: make a list with the name of the cats
 
+	// catList: document.getElementById('catlist-view'),
+	
+	// cats: controller.getAllCats(),
+	
+	init: function () {
+		// set DOM variables
+		this.catList = document.getElementById('catlist-view');
+		this.cats = controller.getAllCats();
+		
+		// render for the first time
+		this.render();
+	},
+	
+	render: function () {
+		// create the cats' list
+		for (let cat of this.cats) {
+			// create and modify <li> items for the cats' list
+			let catListElement = document.createElement('li');
+			catListElement.innerHTML = `See ${cat.name}`;
+			// add each <li> item to the DOM
+			this.catList.appendChild(catListElement);
+		}
+	}
+};
+
+let catView = {
+	
 };
 
 controller.init();
